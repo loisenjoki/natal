d9634adb7bc77b5a7b4881ff2927da75b8b822e8
Loice <lmwai@tutanota.com>
2020-01-16 11:50:31 +0300

    the user deetail home info

--- app/src/main/java/com/luisa/smartnatal/Activities/AdvanceDrawer5Activity.java
+++ app/src/main/java/com/luisa/smartnatal/Activities/AdvanceDrawer5Activity.java
@@ -10,17 +10,24 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.infideap.drawerbehavior.AdvanceDrawerLayout;
+import com.luisa.smartnatal.Fragments.HomeFragment;
+import com.luisa.smartnatal.Fragments.TestFragment;
 import com.luisa.smartnatal.R;
 
 public class AdvanceDrawer5Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private AdvanceDrawerLayout drawer;
+    private boolean viewIsAtHome;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +36,7 @@ public class AdvanceDrawer5Activity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         drawer = (AdvanceDrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -51,16 +51,29 @@ public class AdvanceDrawer5Activity extends AppCompatActivity
         drawer.setRadius(Gravity.START, 35);
         drawer.setViewElevation(Gravity.START, 20);
 
+        displayView(R.id.nav_camera);
+
+
 
     }
 
     @Override
     public void onBackPressed() {
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        if (!viewIsAtHome) { //if the current view is not the News fragment
+            displayView(R.id.nav_camera); //display the News fragment
+        } else {
+            moveTaskToBack(true);  //If view is in News fragment, exit application
+        }
+   /*     if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
-        }
+        }*/
     }
 
 
@@ -68,9 +81,48 @@ public class AdvanceDrawer5Activity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        displayView(item.getItemId());
 
-        drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    public void displayView(int viewId) {
+
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
+
+        switch (viewId) {
+            case R.id.nav_camera:
+                fragment = new HomeFragment();
+                title  = "News";
+                viewIsAtHome = true;
+
+
+                break;
+            case R.id.nav_gallery:
+                fragment = new TestFragment();
+                title = "Events";
+                viewIsAtHome = false;
+
+                break;
+
+        }
+
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        }
+
+        // set the toolbar title
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
     }
 
     @Override
